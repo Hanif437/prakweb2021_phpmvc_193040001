27ab8a79b23874a97d0cad398bd829bc912711b7
Hanif437 <Putrahanif437@gmail.com>
2021-09-30 19:13:09 +0700

    memodifikasi file Home.php di folder app/controller pada video ke 6 di pertemuan ke 4

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -1,11 +1,12 @@
 <?php
 
 class Home extends Controller{
-    public function index()
+    public function index($nama = 'Hanif')
     {
+        $data['nama'] = $nama;
         $data['judul'] = 'Home';
         $this->view('templates/header', $data);
-        $this->view('home/index');
+        $this->view('home/index', $data);
         $this->view('templates/footer');
     }
 }
