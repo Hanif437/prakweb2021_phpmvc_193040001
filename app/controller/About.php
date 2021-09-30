ddd19a74cfd1d0d4b2ed69a8c2f2674b7bbcd672
Hanif437 <Putrahanif437@gmail.com>
2021-09-30 17:56:23 +0700

    memodifikasi file About.php di folder app/controller pada video ke 5 di pertemuan ke 4

--- app/controller/About.php
+++ app/controller/About.php
@@ -1,12 +1,21 @@
 <?php
 
-class About{
-    public function index($nama = 'Hanif', $pekerjaan = 'Mahasiswa')
+class About extends Controller{
+    public function index($nama = 'Hanif', $pekerjaan = 'Mahasiswa', $umur = 20)
     {
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['umur'] = $umur;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
